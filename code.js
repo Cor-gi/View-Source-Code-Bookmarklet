e2cf436957b90e2d99ed22f9ecb26e7538603b38
-Corgi- <99050972+Cor-gi@users.noreply.github.com>
2022-03-29 09:31:13 -0600

    Update code.js

--- code.js
+++ code.js
@@ -3,7 +3,15 @@ var opensourcewin = window.open('about:blank');
 var sourceWin = opensourcewin.document;
 sourceWin.write('
 
-<!DOCTYPE html><html><head><title>Source of ' + location.href + '</title><meta name="viewport" content="width=device-width" /></head><body></body></html>');
+<!DOCTYPE html>
+<html>
+<head>
+<title>Source of ' + location.href + '</title>
+<meta name="viewport" content="width=device-width" />
+</head>
+<body>
+</body>
+</html>');
 var pretext = sourceWin.body.appendChild(sourceWin.createElement("pre"));
     pretext.style.overflow = 'auto';
     pretext.style.whiteSpace = 'pre-wrap';
