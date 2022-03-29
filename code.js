0ab0d207b61afd077587aae00cc7cb228939a555
-Corgi- <99050972+Cor-gi@users.noreply.github.com>
2022-03-29 09:22:13 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,4 +1,3 @@
-window.view_source = function() {
 var opensourcewin = window.open('about:blank');
 var sourceWin = opensourcewin.document;
 sourceWin.write('
@@ -8,4 +7,3 @@ var pretext = sourceWin.body.appendChild(sourceWin.createElement("pre"));
     pretext.style.overflow = 'auto';
     pretext.style.whiteSpace = 'pre-wrap';
     pretext.appendChild(sourceWin.createTextNode(document.documentElement.innerHTML));
-}
